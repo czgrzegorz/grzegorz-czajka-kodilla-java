0c653007f2d0558cd680fc66584e212c8266ccdb
“cz.grzegorz <cz.grzegorz@gmail.com>
2017-12-15 16:52:53 +0100

    Zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,21 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator operation = new Calculator();
+        int addition = operation.doAddition(1,2);
+        int subtraction = operation.doSubstraction(3,2);
+        if (addition == 3){
+            System.out.println("addition OK");
+        } else {
+            System.out.println("error in addition");
+        }
+            if (subtraction == 1){
+                System.out.println("subtraction OK");
+            } else {
+                System.out.println("error in subtraction");
+            }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+
+public class Calculator {
+    public int doAddition(int a, int b) {
+        int c = a+b;
+        System.out.println(c);
+        return c;
+
+    }
+    public int doSubstraction(int a, int b) {
+        int d = a-b;
+        System.out.println(d);
+        return d;
+
+    }
+}
