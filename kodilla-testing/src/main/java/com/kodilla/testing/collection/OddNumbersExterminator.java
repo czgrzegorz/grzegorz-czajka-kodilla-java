4c0c6773f796c6b3bb87fe73fe2946a8c031bcc6
“cz.grzegorz <cz.grzegorz@gmail.com>
2017-12-16 17:37:38 +0100

    testy

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,10 +1,13 @@
 package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class OddNumbersExterminator {
     public static ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
-        for(Integer number: numbers) {
+        Iterator<Integer> iteratorForRemoving = numbers.iterator();
+        while(iteratorForRemoving.hasNext()) {
+            Integer number = iteratorForRemoving.next();
             if (number % 2 != 0) {
                 numbers.remove(number);
             }
